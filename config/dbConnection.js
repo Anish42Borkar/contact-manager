c728fde0c3a89d515558f4204313ea1c1b07769f
anish42borkar <anishborkar42@gmail.com>
2024-10-28 08:41:58 +0530

    minor changes

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,12 +2,10 @@ import mongoose from "mongoose";
 
 const connect = async () => {
   try {
-    console.log(process.env.CONNECTION_STRING, process.env.PORT);
     const connect = await mongoose.connect(process.env.CONNECTION_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-
     console.log("connected ", connect.connection.host, connect.connection.name);
   } catch (error) {
     console.log("failed to connect to database ", error);
